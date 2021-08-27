c8aef3e91b1aee2708bee7b58194b900a0bf8f63
Sahadat Hossain <shahincsejnu@gmail.com>
2021-08-27 19:05:30 +0600

    Add terraform remote backend example
    Signed-off-by: Sahadat Hossain <shahincsejnu@gmail.com>

---
+++ terraform-remote-backend-examples/README.md
@@ -0,0 +1,6 @@
+# Terraform Remote Backend
+
+## Resources
+
+- [x] [Terraform Remote Backend Demystified](https://faun.pub/terraform-remote-backend-demystified-cb4132b95057)
+
---
+++ terraform-remote-backend-examples/example-1-with-aws-s3/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.42.0"
+    }
+  }
+}
+
+variable "access_key" {}
+variable "secret_key" {}
+
+provider "aws" {
+  access_key = var.access_key
+  region = "us-east-2"
+  secret_key = var.secret_key
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-remote-backend-kubeform"
+    key = "kubeform/example-1/terraform.tfstate"
+    region = "us-east-2"
+    encrypt = true
+  }
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "terraform-remote-backend-kubeform"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
